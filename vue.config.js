200ef2f3fd44cc248e5f04de5944f29792b7d958
Gabriel <gabdio02@hotmail.com>
2021-10-13 22:43:26 -0300

    config 2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/https://github.com/gabrieldionne/webpage/'
-    : '/'
+    publicPath:'/https://github.com/gabrieldionne/webpage/'
 }

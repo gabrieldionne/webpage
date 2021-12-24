c5c4579717733df62d1660474d83cefab80eb113
Gabriel <gabdio02@hotmail.com>
2021-12-24 13:26:28 -0400

    added base card for projects

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <About msg="Gabriel Dionne"/>
   <Socials/>
+  <projects :projects="storedProjects"></projects>
 </template>
 
 <script>
-import Socials from './components/Socials.vue'
-import About from './components/About.vue'
+import Socials from './components/Socials.vue';
+import About from './components/About.vue';
+import Projects from './components/Projects.vue';
 
 export default {
   name: 'App',
   components: {
     About,
-    Socials
+    Socials,
+    Projects
+  },
+  data() {
+    return {
+      storedProjects: [
+        {
+          id: 'project1',
+          title: 'testproject1',
+          description: 'first project'
+        },
+        {
+          id: 'project2',
+          title: 'testproject2',
+          description: 'second project'
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/Projects.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul>
+    <project-item 
+      v-for="proj in projects" 
+      :key="proj.id" 
+      :title="proj.title" 
+      :description="proj.description">
+    </project-item>
+  </ul>
+</template>
+
+<script>
+    import ProjectItem from './ItemProject.vue';
+
+    export default{
+        props: ['projects'],
+
+        components: {
+            ProjectItem
+        } 
+    }
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
